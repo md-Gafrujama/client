b09c0bf70ca85a81abae40843be074112e4d87e0
MD GAFRUJAMA ANSARI <156898012+md-Gafrujama@users.noreply.github.com>
2025-10-30 23:38:00 +0530

    added space

--- src/components/admin-view/header.jsx
+++ src/components/admin-view/header.jsx
@@ -11,6 +11,8 @@ function AdminHeader({ setOpen }) {
     
   }
 
+  
+
   return (
     <header className="flex items-center justify-between px-4 py-3 bg-background border-b">
       <Button onClick={() => setOpen(true)} className="lg:hidden sm:block">
